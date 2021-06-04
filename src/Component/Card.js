8f0503c900a2f716344f6d033763d394ad5613af
wiryamanto <wiryamantoragil@gmail.com>
2021-06-04 18:06:15 +0700

    changes pict

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -27,7 +27,7 @@ export default class Cardd extends Component {
                 <Card style={{ width: "18rem" }}>
                   <Card.Img
                     variant="top"
-                    src="https://asset.kompas.com/crops/AnzPGqRxpoD-26G__5fPh6ooZSk=/0x28:640x455/750x500/data/photo/2020/12/12/5fd44cf8e94b1.jpg"
+                    src="https://www.logolynx.com/images/logolynx/1a/1a8eb0c4e61a333a3228e2a87a73aa6f.jpeg"
                   />
                   <Card.Body>
                     <Card.Title>{film.judul}</Card.Title>
